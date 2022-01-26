7b00ef4cbd1ec13d6b42e9d92593aa98d14e45dd
Gitriant <yogitrianto7@gmail.com>
2022-01-26 18:47:12 +0700

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'login_page.dart';
+import 'pages/home_page.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Catalog'),
-          centerTitle: true,
-        ),
-        body: Text('A'),
-      ),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      darkTheme: ThemeData(brightness: Brightness.dark), initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
