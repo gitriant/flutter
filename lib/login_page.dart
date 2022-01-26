a56eb56be9d58385eaf04d1279a82e779c78bd88
Gitriant <yogitrianto7@gmail.com>
2022-01-26 21:01:43 +0700

    Day 5 Completed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,52 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Center(
-            child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Welcome',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+        child: SingleChildScrollView(
+          child: Center(
               child: Column(
-                children: [
-                  TextFormField(
-                      decoration: InputDecoration(
-                          labelText: "Username", hintText: "Enter Username")),
-                  TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          labelText: "Password", hintText: "Enter Password")),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        print("halo");
-                      },
-                      child: Text("Sign In"))
-                ],
+            children: [
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.cover,
               ),
-            )
-          ],
-        )));
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Welcome',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                child: Column(
+                  children: [
+                    TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "Username", hintText: "Enter Username")),
+                    TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: "Password", hintText: "Enter Password")),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: Text("Sign In"),
+                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )),
+        ));
   }
 }
